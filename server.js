6f66e1fe5fdbe1023e1958ae7338ab65b2ea08fa
MUHAMMED AFTHAL K <muhammedafthal786gmail.com@Muhammeds-Air.lan>
2025-02-25 21:41:22 -0800

    Just_Complete_User_Authentication_From_Backend

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/DB.js';
 import foodRouter from './routes/foodRoute.js';
+import userRouter from './routes/userRoute.js';
+import 'dotenv/config';
 
 // app config
 const app = express();
@@ -17,6 +19,7 @@ connectDB();
 // API-Endpoint
 app.use('/api/food',foodRouter);
 app.use('/images', express.static('uploads'));
+app.use('/api/user', userRouter);
 
 app.get('/', (req, res) => {
     res.send("API Is Working...")
